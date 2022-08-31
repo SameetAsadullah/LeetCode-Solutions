64cd0fc05d23348a879e0e928b7625064eb63190
Sameet Asadullah <sameetassadullah744@gmail.com>
2022-08-31 13:33:56 +0500

    Update twoSum.java

--- Two-Sum-Problem/twoSum.java
+++ Two-Sum-Problem/twoSum.java
@@ -3,10 +3,10 @@ class Solution {
         Map<Integer, Integer> valueMap = new HashMap<>(); // value : index
         for (int i = 0; i < nums.length; ++i) {
             int diff = target - nums[i];
-            if (valueMap.containsKey(diff)) {
+            if (valueMap.containsKey(diff)) {   // if difference found
                 return new int[] { valueMap.get(diff), i};
             }
-            valueMap.put(nums[i], i);
+            valueMap.put(nums[i], i);   // if difference not found
         }
         return null; // no solution
     }
