1959fd45027da86bcb4405cc333362daca71ced3
Sameet Asadullah <sameetassadullah744@gmail.com>
2022-08-31 12:45:43 +0500

    Python Solution

---
+++ Two-Sum-Problem/twoSum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        valueMap = {}    # value : index
+        for index, value in enumerate(nums):
+            diff = target - value
+            if diff in valueMap:
+                return [valueMap[diff], index]
+            valueMap[value] = index
